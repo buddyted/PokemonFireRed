b54d138e855700dc3d160fb7146130e61518cd55
Rohan bhujbal <rohanbhujbal2001@gmail.com>
2024-04-08 19:22:04 +0100

    Project created

---
+++ img/Pallet Town.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Pallet Town" tilewidth="16" tileheight="16" tilecount="1472" columns="64">
+ <image source="../Game Boy Advance - Pokemon FireRed LeafGreen - Pallet Town.png" width="1032" height="376"/>
+</tileset>
---
+++ index.js
@@ -0,0 +1,15 @@
+const canvas=  document.querySelector('canvas')
+
+const c = canvas.getContext('2d')
+
+canvas.width = 1024
+canvas.height = 576
+
+c.fillStyle = 'white'
+c.fillRect(0, 0, canvas.width, canvas.height)
+
+const image = new Image()
+image.src = './img/map.png'
+console.log(image)
+
+c.drawImage(image, 0, 0)
